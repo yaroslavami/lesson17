d264def2ab932363b96ffa9b95a56b8e89419168
Yaroslava Mishchenko <yaroslavamishsce@gmail.com>
2023-08-21 11:53:02 -0400

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+let raduga = prompt('скільки кольорів у райдузі?');
+if (raduga == 7) {
+    alert('так і є!')
+} else {
+    alert('я не впевнений у вашій вiдповіді')
+}
+
+
+const animal = ['cat', 'dog', 'lemur', 'lion', 'elephant'];
+
+for (let i = 0; i < animal.length; i++ ){
+   console.log('awesome', animal[i])
+}
+const animals = ['cat', 'dog', 'sun', 'case', 'pen'];
+
+function trans(arr) {
+    for(let i = 0; i < animal.length; i++ ){
+        console.log('awesome', animals[i])
+}
+}
+trans(['cat', 'dog', 'sun', 'case', 'pen']);
